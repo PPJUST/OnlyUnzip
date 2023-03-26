9cb781891a7eef69504335810c3056296ddfb00f
PPJUST <106465946+PPJUST@users.noreply.github.com>
2023-03-26 16:26:16 +0800

    1. 可以解压分卷压缩包（7z、rar、zip）2. 增加了测试压缩结果功能（按大小判断）

--- only_unzip.py
+++ only_unzip.py
@@ -9,7 +9,6 @@ import re
 import random
 import natsort
 import shutil
-import multiprocessing
 import winshell
 
 
@@ -66,12 +65,15 @@ class only_unzip(QMainWindow):
                 drop_files.append(i)
 
         self.icon_change('unzip_star')
-        files_number = len(drop_files)  # 总文件数
+        fenjuan_full_dict, no_fenjuan_files = self.check_fenjuan_zip(drop_files)  # 调用检查分卷函数，分离分卷文件和普通文件
+
+        files_number = len(no_fenjuan_files) + len(fenjuan_full_dict)  # 总文件数
         rate_file_number = 0  # 到第几个文件了
         error_number = 0  # 解压失败的文件数
-        zip_path = './7-Zip/7z.exe'  # 设置7z指令
+        damage_number = 0  # 损坏的压缩文件数
+        zip_path = './7-Zip/7z.exe'  # 设置7zip指令
 
-        for file in drop_files:  # 开始逐个处理压缩文件
+        for file in no_fenjuan_files:  # 开始逐个处理压缩文件（先处理不是分卷的文件）
             rate_file_number += 1
             self.info_text_change(rate_file_number, files_number)
             # 设置7z的指令
@@ -87,19 +89,152 @@ class only_unzip(QMainWindow):
                 if self.result.returncode != 0:
                     password_try_number += 1  # 返回码不为0则解压失败，密码失败次数+1
                 elif self.result.returncode == 0:
-                    # send2trash.send2trash(file)
-                    winshell.delete_file(file, no_confirm=True)  # 删除原文件到回收站
-                    self.right_password_number_add_one(password)  # 成功解压则密码使用次数+1
-                    self.check_unzip_result(unzip_path, file_directory, temporary_folder)  # 执行检查函数，并传递需要的变量
-
-                    break  # 检查完结果后退出循环
+                    # 检查解压结果和原文件大小比较
+                    original_size = os.path.getsize(file)  # 原文件大小
+                    unzip_size = self.get_folder_size(unzip_path)  # 压缩结果大小
+                    if unzip_size < original_size * 0.9:  # 解压后文件大小如果小于原文件90%则说明压缩包损坏
+                        winshell.delete_file(unzip_path, no_confirm=True)  # 删除解压结果
+                        damage_number += 1  # 计数+1
+                        break  # 退出当前文件循环
+                    else:
+                        # send2trash.send2trash(file)
+                        winshell.delete_file(file, no_confirm=True)  # 删除原文件到回收站
+                        self.right_password_number_add_one(password)  # 成功解压则密码使用次数+1
+                        self.check_unzip_result(unzip_path, file_directory, temporary_folder)  # 执行检查函数，并传递需要的变量
+                        break  # 检查完结果后退出循环
+            if password_try_number == len(passwords):
+                error_number += 1
+        for first_fenjuan_file in fenjuan_full_dict.keys():  # 然后处理分卷压缩包，解压键，成功的话再删除值
+            '''
+            下面的处理代码基本与上面的非分卷处理代码相同，大部分只在删除文件的地方有改动
+            改动的地方用★标注
+            '''
+            rate_file_number += 1
+            self.info_text_change(rate_file_number, files_number)
+            # 设置7z的指令
+            file_directory = os.path.split(first_fenjuan_file)[0]  # 文件的父目录
+            file_name_without_suffix = self.draw_fenjuan_prefix(first_fenjuan_file)  # 单独的没有后缀的文件名★
+            temporary_folder = os.path.join(file_directory, "unzipTempFolder")  # 临时存放解压结果的文件夹
+            unzip_path = os.path.join(temporary_folder, file_name_without_suffix)  # 解压到临时文件下与文件同名的文件夹中
+            password_try_number = 0  # 密码尝试次数
+            for password in passwords:
+                zip_command = [zip_path, "x", "-p" + password, "-y", first_fenjuan_file, "-o" + unzip_path]  # 组合完整7z指令
+                # self.unzip_start(zip_command)
+                self.unzip_start(zip_command)
+                if self.result.returncode != 0:
+                    password_try_number += 1  # 返回码不为0则解压失败，密码失败次数+1
+                elif self.result.returncode == 0:
+                    # 检查解压结果和原文件大小比较
+                    original_size = 0
+                    for x in fenjuan_full_dict[first_fenjuan_file]:
+                        original_size += os.path.getsize(x)  # 原文件大小(各个分卷合并）★
+                    unzip_size = self.get_folder_size(unzip_path)  # 压缩结果大小
+                    if unzip_size < original_size * 0.9:  # 解压后文件大小如果小于原文件90%则说明压缩包损坏
+                        winshell.delete_file(unzip_path, no_confirm=True)  # 删除解压结果
+                        damage_number += 1  # 计数+1
+                        break  # 退出当前文件循环
+                    else:
+                        # send2trash.send2trash(file)
+                        print('输出测试')
+                        print(fenjuan_full_dict[first_fenjuan_file])
+                        for x in fenjuan_full_dict[first_fenjuan_file]:
+                            print(x)
+                            winshell.delete_file(x, no_confirm=True)  # 删除各个压缩文件到回收站★
+                        self.right_password_number_add_one(password)  # 成功解压则密码使用次数+1
+                        self.check_unzip_result(unzip_path, file_directory, temporary_folder)  # 执行检查函数，并传递需要的变量
+                        break  # 检查完结果后退出循环
             if password_try_number == len(passwords):
                 error_number += 1
         if len(os.listdir(temporary_folder)) == 0:  # 解压完成后如果临时文件夹为空，则删除
             # send2trash.send2trash(temporary_folder)
             winshell.delete_file(temporary_folder, no_confirm=True)
         self.icon_change('unzip_finish')
-        self.ui.label_info.setText(f'成功：{files_number - error_number}，失败：{error_number}')
+        self.ui.label_info.setText(f'成功:{files_number - error_number} 失败:{error_number} 损坏:{damage_number}')
+
+    def get_folder_size(self, folder):
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk(folder):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                total_size += os.path.getsize(fp)
+        return total_size
+
+    def check_fenjuan_zip(self, files):
+        """检查分卷压缩包"""
+        new_files = [x for x in files]  # 复制列表，最后会输出不包含分卷的列表
+        re_rar = "^(.+)\.part\d+\.rar$"
+        re_7z = "^(.+)\.7z\.\d+$"
+        re_zip_top = "^(.+)\.zip$"
+        re_zip_other = "^(.+)\.z\d+$"
+        fenjuan_full_dict = {}  # 分卷文件字典（键值对为 第一个分卷：全部分卷）
+        for i in files:
+            if re.match(re_7z, i):  # 匹配7z正则
+                prefix = re.match(re_7z, i).group(1) + r'.7z.001'  # 设置字典的键（提取的正则前缀+手工添加后缀）
+                if prefix not in fenjuan_full_dict:  # 如果文件名不在字典内，则添加一个空键值对
+                    fenjuan_full_dict[prefix] = set()  # 用集合添加（目的是为了后面的zip分卷，其实用列表更方便）
+                fenjuan_full_dict[prefix].add(i)  # 添加键值对（示例.7z.001：示例.7z.001，示例.7z.002）
+                new_files.remove(i)  # 将新列表中的分卷压缩包剔除
+            elif re.match(re_zip_other, i) or re.match(re_zip_top, i):  # 只要是zip后缀的，都视为分卷压缩包
+                if re.match(re_zip_other, i):
+                    prefix = re.match(re_zip_other, i).group(1) + r'.zip'
+                else:
+                    prefix = re.match(re_zip_top, i).group(1) + r'.zip'
+                if prefix not in fenjuan_full_dict:
+                    fenjuan_full_dict[prefix] = set()
+                fenjuan_full_dict[prefix].add(i)
+                fenjuan_full_dict[prefix].add(prefix)  # zip分卷的特性，第一个分卷包名称是.zip后缀
+                new_files.remove(i)
+                if prefix in new_files:  # zip分卷特性，如果是分卷删除第一个.zip后缀的文件名
+                    new_files.remove(prefix)
+                else:
+                    pass
+            elif re.match(re_rar, i):
+                prefix = re.match(re_rar, i).group(1) + r'.part1.rar'
+                if prefix not in fenjuan_full_dict:
+                    fenjuan_full_dict[prefix] = set()
+                fenjuan_full_dict[prefix].add(i)
+                new_files.remove(i)
+        # 检查没有导入的压缩包分卷（例如files只包括其中某几个分卷）
+        one_of_files = files[0]
+        print(one_of_files)
+        all_files = os.listdir(os.path.split(one_of_files)[0])
+        dir_file_list = []
+        for i in all_files:
+            dir_file_list.append(os.path.join(os.path.split(one_of_files)[0] + '/' + i))
+        # 假设使用的是完整文件名（文件路径+文件名+后缀）
+        for i in dir_file_list:
+            if re.match(re_7z, i):  # 匹配7z正则
+                prefix = re.match(re_7z, i).group(1) + r'.7z.001'
+                print('---测试')
+                print(prefix)
+                print(fenjuan_full_dict)
+                print('---测试')
+                if prefix in fenjuan_full_dict:
+                    fenjuan_full_dict[prefix].add(i)
+            elif re.match(re_zip_other, i):
+                prefix = re.match(re_zip_other, i).group(1) + r'.zip'
+                if prefix in fenjuan_full_dict:
+                    fenjuan_full_dict[prefix].add(i)
+            elif re.match(re_rar, i):
+                prefix = re.match(re_rar, i).group(1) + r'.part1.rar'
+                if prefix in fenjuan_full_dict:
+                    fenjuan_full_dict[prefix].add(i)
+        return fenjuan_full_dict, new_files  # fenjuan_full_dict：分卷文件，new_files：不包含分卷的文件
+
+    def draw_fenjuan_prefix(self, file_name):
+        """提取分卷文件的文件名（不包含后缀）"""
+        re_rar = "^(.+)\.part\d+\.rar$"
+        re_7z = "^(.+)\.7z\.\d+$"
+        re_zip_top = "^(.+)\.zip$"
+        re_zip_other = "^(.+)\.z\d+$"
+        if re.match(re_7z, file_name):  # 匹配7z正则
+            prefix = re.match(re_7z, file_name).group(1)
+        elif re.match(re_zip_top, file_name):
+            prefix = re.match(re_zip_top, file_name).group(1)
+        elif re.match(re_rar, file_name):
+            prefix = re.match(re_rar, file_name).group(1)
+        prefix_filename = os.path.split(prefix)[1]
+        return prefix_filename
 
     def check_unzip_result(self, unzip_path, file_directory, temporary_folder):
         """检查解压结果"""
@@ -228,5 +363,4 @@ def main():
 
 
 if __name__ == "__main__":
-    multiprocessing.freeze_support()
     main()
