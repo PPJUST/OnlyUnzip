c08921e24b9f3d55282e398ff631ddbba7a4d250
PPJUST <1506142459@qq.com>
2023-05-07 20:47:20 +0800

    更新

--- ui.py
+++ ui.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'ui_v1 ԭ�ļ�vkrwIV.ui'
+## Form generated from reading UI file 'ui_v1 ԭ�ļ�pxjqih.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -33,12 +33,13 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(320, 240)
+        MainWindow.resize(292, 254)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.horizontalLayout = QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.splitter = QSplitter(self.centralwidget)
         self.splitter.setObjectName(u"splitter")
         self.splitter.setFrameShape(QFrame.NoFrame)
@@ -46,22 +47,32 @@ class Ui_MainWindow(object):
         self.splitter.setOrientation(Qt.Vertical)
         self.splitter.setOpaqueResize(True)
         self.splitter.setHandleWidth(0)
+        self.splitter.setChildrenCollapsible(True)
         self.button_page_main = QPushButton(self.splitter)
         self.buttonGroup = QButtonGroup(MainWindow)
         self.buttonGroup.setObjectName(u"buttonGroup")
         self.buttonGroup.addButton(self.button_page_main)
         self.button_page_main.setObjectName(u"button_page_main")
+        self.button_page_main.setMinimumSize(QSize(0, 50))
+        self.button_page_main.setMaximumSize(QSize(16777215, 16777215))
+        self.button_page_main.setAutoRepeat(False)
+        self.button_page_main.setAutoDefault(False)
+        self.button_page_main.setFlat(False)
         self.splitter.addWidget(self.button_page_main)
         self.button_page_password = QPushButton(self.splitter)
         self.buttonGroup.addButton(self.button_page_password)
         self.button_page_password.setObjectName(u"button_page_password")
+        self.button_page_password.setMinimumSize(QSize(0, 50))
+        self.button_page_password.setMaximumSize(QSize(16777215, 16777215))
         self.splitter.addWidget(self.button_page_password)
         self.button_page_setting = QPushButton(self.splitter)
         self.buttonGroup.addButton(self.button_page_setting)
         self.button_page_setting.setObjectName(u"button_page_setting")
+        self.button_page_setting.setMinimumSize(QSize(0, 50))
+        self.button_page_setting.setMaximumSize(QSize(16777215, 16777215))
         self.splitter.addWidget(self.button_page_setting)
 
-        self.horizontalLayout.addWidget(self.splitter)
+        self.horizontalLayout.addWidget(self.splitter, 0, Qt.AlignTop)
 
         self.stackedWidget = QStackedWidget(self.centralwidget)
         self.stackedWidget.setObjectName(u"stackedWidget")
@@ -70,79 +81,90 @@ class Ui_MainWindow(object):
         self.page = QWidget()
         self.page.setObjectName(u"page")
         self.page.setAcceptDrops(False)
+        self.verticalLayout_3 = QVBoxLayout(self.page)
+        self.verticalLayout_3.setSpacing(0)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(6, 6, 6, 0)
         self.label_icon = MyLabel(self.page)
         self.label_icon.setObjectName(u"label_icon")
-        self.label_icon.setGeometry(QRect(9, 9, 191, 131))
         self.label_icon.setAcceptDrops(True)
         self.label_icon.setFrameShape(QFrame.NoFrame)
         self.label_icon.setScaledContents(True)
+
+        self.verticalLayout_3.addWidget(self.label_icon)
+
         self.line_3 = QFrame(self.page)
         self.line_3.setObjectName(u"line_3")
-        self.line_3.setGeometry(QRect(10, 150, 207, 16))
         self.line_3.setFrameShape(QFrame.HLine)
         self.line_3.setFrameShadow(QFrame.Sunken)
-        self.layoutWidget = QWidget(self.page)
-        self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(9, 170, 116, 16))
-        self.horizontalLayout_2 = QHBoxLayout(self.layoutWidget)
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.label_3 = QLabel(self.layoutWidget)
+
+        self.verticalLayout_3.addWidget(self.line_3)
+
+        self.splitter_2 = QSplitter(self.page)
+        self.splitter_2.setObjectName(u"splitter_2")
+        self.splitter_2.setOrientation(Qt.Horizontal)
+        self.label_3 = QLabel(self.splitter_2)
         self.label_3.setObjectName(u"label_3")
-        self.label_3.setMaximumSize(QSize(60, 16777215))
-
-        self.horizontalLayout_2.addWidget(self.label_3)
-
-        self.label_current_file = QLabel(self.layoutWidget)
+        self.label_3.setMaximumSize(QSize(60, 20))
+        self.splitter_2.addWidget(self.label_3)
+        self.label_current_file = QLabel(self.splitter_2)
         self.label_current_file.setObjectName(u"label_current_file")
+        self.splitter_2.addWidget(self.label_current_file)
 
-        self.horizontalLayout_2.addWidget(self.label_current_file)
+        self.verticalLayout_3.addWidget(self.splitter_2)
 
-        self.layoutWidget1 = QWidget(self.page)
-        self.layoutWidget1.setObjectName(u"layoutWidget1")
-        self.layoutWidget1.setGeometry(QRect(10, 190, 116, 16))
-        self.horizontalLayout_3 = QHBoxLayout(self.layoutWidget1)
-        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.label_4 = QLabel(self.layoutWidget1)
+        self.splitter_3 = QSplitter(self.page)
+        self.splitter_3.setObjectName(u"splitter_3")
+        self.splitter_3.setOrientation(Qt.Horizontal)
+        self.label_4 = QLabel(self.splitter_3)
         self.label_4.setObjectName(u"label_4")
-        self.label_4.setMaximumSize(QSize(60, 16777215))
-
-        self.horizontalLayout_3.addWidget(self.label_4)
-
-        self.label_schedule = QLabel(self.layoutWidget1)
+        self.label_4.setMaximumSize(QSize(60, 20))
+        self.splitter_3.addWidget(self.label_4)
+        self.label_schedule = QLabel(self.splitter_3)
         self.label_schedule.setObjectName(u"label_schedule")
+        self.splitter_3.addWidget(self.label_schedule)
 
-        self.horizontalLayout_3.addWidget(self.label_schedule)
+        self.verticalLayout_3.addWidget(self.splitter_3)
 
         self.stackedWidget.addWidget(self.page)
         self.page_2 = QWidget()
         self.page_2.setObjectName(u"page_2")
-        self.verticalLayout = QVBoxLayout(self.page_2)
-        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout_2 = QVBoxLayout(self.page_2)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.text_password = QPlainTextEdit(self.page_2)
         self.text_password.setObjectName(u"text_password")
 
-        self.verticalLayout.addWidget(self.text_password)
+        self.verticalLayout_2.addWidget(self.text_password)
 
-        self.splitter_3 = QSplitter(self.page_2)
-        self.splitter_3.setObjectName(u"splitter_3")
-        self.splitter_3.setOrientation(Qt.Horizontal)
-        self.button_update_password = QPushButton(self.splitter_3)
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.button_update_password = QPushButton(self.page_2)
         self.button_update_password.setObjectName(u"button_update_password")
-        self.splitter_3.addWidget(self.button_update_password)
-        self.splitter_2 = QSplitter(self.splitter_3)
-        self.splitter_2.setObjectName(u"splitter_2")
-        self.splitter_2.setOrientation(Qt.Vertical)
-        self.button_export_password = QPushButton(self.splitter_2)
-        self.button_export_password.setObjectName(u"button_export_password")
-        self.splitter_2.addWidget(self.button_export_password)
-        self.button_export_password_with_number = QPushButton(self.splitter_2)
-        self.button_export_password_with_number.setObjectName(u"button_export_password_with_number")
-        self.splitter_2.addWidget(self.button_export_password_with_number)
-        self.splitter_3.addWidget(self.splitter_2)
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.button_update_password.sizePolicy().hasHeightForWidth())
+        self.button_update_password.setSizePolicy(sizePolicy)
 
-        self.verticalLayout.addWidget(self.splitter_3)
+        self.horizontalLayout_4.addWidget(self.button_update_password)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.button_export_password = QPushButton(self.page_2)
+        self.button_export_password.setObjectName(u"button_export_password")
+
+        self.verticalLayout.addWidget(self.button_export_password)
+
+        self.button_export_password_with_number = QPushButton(self.page_2)
+        self.button_export_password_with_number.setObjectName(u"button_export_password_with_number")
+
+        self.verticalLayout.addWidget(self.button_export_password_with_number)
+
+
+        self.horizontalLayout_4.addLayout(self.verticalLayout)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_4)
 
         self.stackedWidget.addWidget(self.page_2)
         self.page_3 = QWidget()
@@ -207,7 +229,8 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.stackedWidget.setCurrentIndex(2)
+        self.button_page_main.setDefault(False)
+        self.stackedWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
