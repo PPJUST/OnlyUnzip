08edbb989be6dabd5af43cea77e8a8be6926a8f4
ppjust <ppjust@>
2024-01-30 16:13:34 +0800

    更新

--- qthread_7zip_new.py
+++ qthread_7zip_new.py
@@ -42,14 +42,22 @@ class Thread7z(QThread):
         for index, file in enumerate(self.file_dict.keys(), start=1):
             # 不论哪个模式，都先使用7z的l命令进行测试
             # 先使用临时密码测试，如果返回成功则说明该文件无法使用l指令进行正常测试（无密码或者内部文件名未加密）
+            # 如果返回其余情况，则按不同情况进行单独处理
             right_password_by_l = _PASSWORD_NONE  # 尝试使用l指令寻找的正确密码
-            result = function_archive.subprocess_run_7z('l', file, _PASSWORD_NONE)
-            if type(result) is not State7zResult.Success:  # 不成功则用l命令继续测试密码
+            result_fake = function_archive.subprocess_run_7z('l', file, _PASSWORD_NONE)
+            if type(result_fake) is  State7zResult.WrongPassword:  # 返回密码错误。则继续使用密码字典寻找正确密码
                 test_passwords = function_password.read_passwords()
                 for password in test_passwords:
                     result = function_archive.subprocess_run_7z('l', file, password)
                     if type(result) is State7zResult.Success:  # 找到正确密码
                         right_password_by_l = result.password
+                        break
+            elif type(result_fake) is  State7zResult.FileOccupied:  # 返回文件占用，则不再处理该文件
+                pass  # 备忘录
+            elif type(result_fake) is State7zResult.NotArchiveOrDamaged:
+                pass
+            elif type(result_fake) is State7zResult.UnknownError:
+                pass
 
             if right_password_by_l != _PASSWORD_NONE:  # 如果找到了正确密码，则直接按正确密码执行
                 if mode == 'test':
@@ -59,10 +67,13 @@ class Thread7z(QThread):
             else:  # 未找到则按正常流程执行
                 pass
 
+    def extract_archive(self):
+        """解压文件"""
 
 
-    def extract_by_7z(self, file, password, exclude_rules,output_folder=None):
-        """使用popen方法调用7z，并实时发送进度信息"""
+
+    def process_7z_extract(self, file, password, exclude_rules,output_folder=None):
+        """使用popen方法调用7z进行解压操作，并实时发送进度信息"""
         # 同时读取stdout和stderr会导致管道堵塞，需要将这3个流重定向至1个管道中，使用switch 'bso1','bsp1',bse1'
         if output_folder:
             output_folder = os.path.normpath(os.path.join(output_folder,_Unzip_Temp_Folder))  # 输出的临时文件夹
