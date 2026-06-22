877383a2ffb1458fde4aef4984a1a3fcf35b9e8e
PPJUST <106465946+ppjust@users.noreply.github.com>
2026-06-22 19:44:28 +0800

    优化：更新文本

--- components/page_about/viewer.py
+++ components/page_about/viewer.py
@@ -28,9 +28,9 @@ class AboutViewer(QWidget):
 
     def set_info(self):
         # 版本号
-        self.ui.label_version.setText('v2.1.2')
+        self.ui.label_version.setText('v2.1.3')
         # 编译日期
-        self.ui.label_date.setText('2026.05.09')
+        self.ui.label_date.setText('2026.06.22')
         # 项目主页
         self.ui.label_project.setText('<a href="https://github.com/PPJUST/OnlyUnzip">Github</a>')
         # 下载地址
